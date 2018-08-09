312e0ac0e1c0191057cc3338d4df9da54705a94b
keduoc <39652079+keduoc@users.noreply.github.com>
2018-08-10 00:10:58 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+define(['top250', 'usBox', 'search', 'jquery'], function(Top250Page, UsBoxPage, SearchPage){
+    var App = {
+        init: function(){
+            this.bind()
+            Top250Page.init()
+            UsBoxPage.init()
+            SearchPage.init()
+        },
+        bind: function(){
+            $('footer>div').click(function(){
+                $(this).addClass('active')
+                    .siblings()
+                    .removeClass('active')
+                $currentPage = $('main>section')
+                    .hide().eq($(this).index())
+                    .fadeIn()
+            })
+            window.ontouchmove = function(e){
+                e.preventDefault()
+            }
+            $('section').each(function(){
+                this.ontouchmove = function(e){
+                    e.stopPropagation()
+                }
+            })
+        }
+    }
+    return App
+
+})
